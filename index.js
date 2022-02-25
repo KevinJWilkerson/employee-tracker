9211b73af000e91df4ae43c01edbdb89cd9a8e74
Kevin <kevin.j.wilkerson@gmail.com>
2022-02-25 17:51:15 -0600

    added new role, new employee, and update employee role functions

--- index.js
+++ index.js
@@ -116,13 +116,135 @@ const addDepartment = () => {
     ])
     .then(({ name }) => {
       const sql = `
-        INSERT INTO departments (name) VALUES ('${name}')`;
+        INSERT INTO departments (name)
+        VALUES (
+            '${name}')`;
       db.query(sql, (err, rows) => {
         if (err) {
           console.log(err);
           return err;
         }
-        console.log(`${name} added as a new department.`);
+        console.log(`${name} has been added as a new department.`);
+        promptUser();
+      });
+    });
+};
+
+const addRole = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "text",
+        name: "title",
+        message: "Enter the title of the new role.",
+      },
+      {
+        type: "text",
+        name: "department_id",
+        message: "Enter the Department ID for this new role.",
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "Enter the salary for the new role",
+      },
+    ])
+    .then((answers) => {
+      const sql = `
+          INSERT INTO roles (title, department_id, salary)
+          VALUES (
+            '${answers.title}',
+            '${answers.department_id}',
+            '${answers.salary}')`;
+      db.query(sql, (err, rows) => {
+        if (err) {
+          console.log(err);
+          return err;
+        }
+        console.log(`${answers.title} has been added as a new role.`);
+        promptUser();
+      });
+    });
+};
+
+const addEmployee = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "text",
+        name: "first_name",
+        message: "Enter the new employee's first name.",
+      },
+      {
+        type: "text",
+        name: "last_name",
+        message: "Enter the new employee's last name.",
+      },
+      {
+        type: "input",
+        name: "manager_id",
+        message: "Enter the manager ID for the new employee.",
+      },
+      {
+        type: "input",
+        name: "role_id",
+        message: "Enter the role ID for the new employee.",
+      },
+    ])
+    .then((answers) => {
+      const sql = `
+            INSERT INTO employees (first_name, last_name, manager_id, role_id)
+            VALUES (
+                '${answers.first_name}',
+                '${answers.last_name}',
+                '${answers.manager_id}',
+                '${answers.role_id}')`;
+      db.query(sql, (err, rows) => {
+        if (err) {
+          console.log(err);
+          return err;
+        }
+        console.log(
+          `${answers.first_name} ${answers.last_name} has been added as a new employee.`
+        );
+        promptUser();
+      });
+    });
+};
+
+const updateEmployeeRole = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "text",
+        name: "first_name",
+        message: "Enter the employee's first name.",
+      },
+      {
+        type: "text",
+        name: "last_name",
+        message: "Enter the employee's last name.",
+      },
+      {
+        type: "input",
+        name: "role_id",
+        message: "Enter the role ID for the employee's new role.",
+      },
+    ])
+    .then((answers) => {
+      const sql = `
+              UPDATE employees
+              SET role_id = '${answers.role_id}'
+              WHERE first_name = '${answers.first_name}'
+              AND last_name = '${answers.last_name}'`;
+      db.query(sql, (err, rows) => {
+        if (err) {
+          console.log(err);
+          return err;
+        }
+        console.log(
+          `${answers.first_name} ${answers.last_name}'s role ID has been updated to ${answers.role_id}.`
+        );
         promptUser();
       });
     });
